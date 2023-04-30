39df092ad2d5f559231e7c6eea6a92687358cd9c
bmbolekwa <bmbolekwa@worldremit.com>
2023-04-30 20:06:14 +0200

    first commit

---
+++ src/main/java/io/zepz/chat/ChatServiceApiTutApplication.java
@@ -0,0 +1,13 @@
+package io.zepz.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServiceApiTutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServiceApiTutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/zepz/chat/ChatServiceApiTutApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zepz.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApiTutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
